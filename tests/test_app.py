662d9e5c3b6eb00a65b6327ab81f06e16d7382e5
rbadrin <69078390+rbadrin@users.noreply.github.com>
2021-11-07 09:13:47 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,6 @@
 from app import index
-from app import getWeather
-import requests
+
 
 def test_index():
-	assert index() == "<h1>Hello world!!</h1><br> Use '/getWeather' to nav to weather api</p>"
+	assert index() == "<h1>Hello world!!</h1>"
 
-def test_getWeather():
-	r = requests.get("https://api.openweathermap.org/data/2.5/weather?q=chennai&appid=06c921750b9a82d8f5d1294e1586276f")
-	assert r.status_code == 200
